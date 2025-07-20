9f7e85ec16dab3785d59c14518f68bceb075de8e
Zachary Todd <github@zttodd.com>
2025-07-20 18:49:35 -0400

    Set up separate PostCSS config; Nunjucks layouts and partials

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,12 @@
 export default function (eleventyConfig) {
   // Watch targets
-  eleventyConfig.addWatchTarget('src/css/**/*.css');
+  eleventyConfig.addWatchTarget('src/_assets/css/**/*.css');
 
   // Copy files
-  eleventyConfig.addPassthroughCopy("src/*.js");
+  eleventyConfig.addPassthroughCopy("src/_assets/js/**/*.js");
   eleventyConfig.addPassthroughCopy("src/*.ico");
 
   // Input directory
   eleventyConfig.setInputDirectory("src");
+  eleventyConfig.setIncludesDirectory("_includes");
 };
---
+++ src/index.md
@@ -0,0 +1,9 @@
+---
+layout: layouts/base.njk
+title: Zachary Todd
+metaDescription: "Personal website of Zachary Todd"
+---
+
+<div class="heading">
+    <h1>Zachary Todd</h1>
+</div>
